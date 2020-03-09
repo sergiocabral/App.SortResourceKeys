8c7c6a7376459ef42d526844d7f7eb8c8d6ddc41
Sergio Cabral <sergio@cabral.br.com>
2020-03-08 22:46:45 -0300

    Ordenação do código C#.

--- Program.cs
+++ Program.cs
@@ -18,8 +18,8 @@ class Program
             var fileResxContent = File.ReadAllLines(fileResx.FullName, Encoding.UTF8);
             var fileCSharpContent = fileCSharp.Exists ? File.ReadAllLines(fileCSharp.FullName, Encoding.UTF8) : new string[0];
 
-            fileResxContent = SortResx(fileResxContent);
             fileCSharpContent = SortCSharp(fileCSharpContent);
+            fileResxContent = SortResx(fileResxContent);
 
             try
             {
@@ -33,29 +33,100 @@ class Program
         }
     }
 
+    private enum Mode
+    {
+        Header,
+        Body,
+        Footer
+    }
+
     private static string[] SortCSharp(string[] lines)
     {
         var header = new List<string>();
-        var body = new List<string>();
+        var body = new List<List<string>>(); 
         var footer = new List<string>();
-        
-        
-        
+
+        var mode = Mode.Header;
+        var i = 0;
+
+        foreach (var line in lines)
+        {
+            switch (mode)
+            {
+                case Mode.Header:
+                    if (!Regex.IsMatch(line, @"public\s*static\s*string\s*[^ ]*\s*{"))
+                    {
+                        header.Add(line);
+                    }
+                    else
+                    {
+                        body.Add(new List<string>());
+                        
+                        while (header[header.Count - 1].Trim() != "}")
+                        {
+                            body[body.Count - 1].Insert(0, header[header.Count - 1]);
+                            header.RemoveAt(header.Count - 1);
+                        }
+
+                        mode = Mode.Body;
+                        goto case Mode.Body;
+                    }
+                    
+                    break;
+                case Mode.Body:
+                    if (line.Trim() == "}") i++;
+                    else i = 0;
+
+                    if (i <= 2)
+                    {
+                        body[body.Count - 1].Add(line);
+                    }
+                    if (i == 2)
+                    {
+                        body.Add(new List<string>());
+                    }
+                    
+                    if (i > 2)
+                    {
+                        body.RemoveAt(body.Count - 1);
+                        mode = Mode.Footer;
+                        goto case Mode.Footer;
+                    }
+                    
+                    break;
+                case Mode.Footer:
+                    footer.Add(line);
+                    break;
+            }
+        }
+
         return Join(header, body, footer);
     }
 
     private static string[] SortResx(string[] lines)
     {
-        var header = new List<string>();
-        var body = new List<string>();
+        var header = new List<string>(); 
+        var body = new List<List<string>>(); 
         var footer = new List<string>();
         
         return Join(header, body, footer);
     }
 
-    private static string[] Join(List<string> header, List<string> body, List<string> footer)
+    private static string[] Join(List<string> header, List<List<string>> body, List<string> footer)
     {
-        header.AddRange(body);
+        body.Sort((list1, list2) =>
+        {
+            const string regex = @"(?<=public\s*static\s*string\s*)[^ ]*(?=\s*{)";
+            
+            var text1 = string.Join(Environment.NewLine, list1.ToArray());
+            var key1 = Regex.Match(text1, regex, RegexOptions.Singleline).Value;
+            
+            var text2 = string.Join(Environment.NewLine, list2.ToArray());
+            var key2 = Regex.Match(text2, regex, RegexOptions.Singleline).Value;
+
+            return string.Compare(key1, key2, StringComparison.Ordinal);
+        });
+        foreach (var block in body) header.AddRange(block);
         header.AddRange(footer);
         return header.ToArray();
     }
